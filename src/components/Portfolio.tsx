5cd5e46bc387908eed80d406afc75df390294c72
fakhriyramadhan25 <fakhriyramadhan25@gmail.com>
2025-02-24 20:37:20 +0700

    changing the portfolio link to cim

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -78,7 +78,7 @@ export default function Portfolio() {
               {
                 name: "Browse",
                 image: SiGooglechrome,
-                link: "",
+                link: "https://cim.pu.go.id/",
               },
             ]}
           >
